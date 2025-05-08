c4fb4e08a28dd75170f25222974995ce12f9bc9e
dan-os <48282179+dan-os@users.noreply.github.com>
2025-05-09 00:20:01 +0100

    Add install indexer

--- install.sh
+++ install.sh
@@ -5,20 +5,20 @@ set -euo pipefail
 export ROOT_DIR="$(git rev-parse --show-toplevel)"
 source $ROOT_DIR/lib/common.sh
 
-printenv
-
 # acquire sudo
 execute "sudo" "-v"
 trap 'sudo -k' EXIT
 
-ohai "Installing devbox..."
-(
-  curl -fsSL https://get.jetify.com/devbox | bash
-  DEVBOX_VERSION=$(devbox version)
-  echo "Installed devbox version: ${DEVBOX_VERSION}"
-) || exit 1
+for src in $(find -H "$DOTFILES_DIR" -maxdepth 2 -name 'bootstrap.sh' -not -path '*.git*'); do
+  ohai "Installing $src"
+  (
+    execute "bash" "$src"
+  ) || exit 1
+done
 
-# install devbox
-# devbox
-# export DEVBOX_USE_VERSION=0.8.0
-# curl -fsSL https://get.jetify.com/devbox | bash
+# ohai "Installing devbox..."
+# (
+#   curl -fsSL https://get.jetify.com/devbox | bash
+#   DEVBOX_VERSION=$(devbox version)
+#   echo "Installed devbox version: ${DEVBOX_VERSION}"
+# ) || exit 1
