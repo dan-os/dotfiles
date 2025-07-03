26eb869b521f77d138eb4042300e8a5b2e83b843
dan-os <48282179+dan-os@users.noreply.github.com>
2025-07-03 19:49:32 +0100

    Update install script location

--- scripts/_bootstrap.sh
+++ scripts/_bootstrap.sh
@@ -1,2 +1,24 @@
-# install chezmoi and apply github@dan-os/dotfiles.git
-sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply dan-os
+#!/bin/sh
+
+# exit on error
+set -e
+
+BIN_DIR="$HOME/.local/bin"
+CHEZMOI="$BIN_DIR/chezmoi"
+
+# install chezmoi
+if [ ! "$(command -v chezmoi)" ]; then
+    if [ "$(command -v curl)" ]; then
+        sh -c "$(curl -fsSL https://get.chezmoi.io)" -- -b "$BIN_DIR"
+    elif [ "$(command -v wget)" ]; then
+        sh -c "$(wget -qO- https://get.chezmoi.io)" -- -b "$BIN_DIR"
+    else
+        echo "CURL or WGET must be installed first!" >&2
+        exit 1
+    fi
+else
+    CHEZMOI=chezmoi
+fi
+
+# apply github@dan-os/dotfiles.git
+exec "$CHEZMOI" init --apply dan-os/dotfiles
